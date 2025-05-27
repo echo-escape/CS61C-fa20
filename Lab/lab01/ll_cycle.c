1d20acbc52165af36f2f68a65627e8a9dc80135b
echo-escape <echo-escape@github.io>
2025-05-27 20:52:13 +0800

    Complete lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if (head == NULL) {
+        return 0; // An empty list cannot have a cycle
+    }
+    node *slow = head;
+    node *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          // Move slow pointer by 1 step
+        fast = fast->next->next;   // Move fast pointer by 2 steps
+
+        if (slow == fast) {         // If they meet, there is a cycle
+            return 1; // Cycle detected
+        }
+    }
+    return 0; // No cycle detected
 }
